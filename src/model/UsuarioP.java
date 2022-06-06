580029ab9e37128d812f2be517fe6c3c4cd7e4b0
Gabriel Soares <64909008+gabrielsoaressn@users.noreply.github.com>
2022-06-06 15:18:14 -0300

    Add files via upload

---
+++ src/model/UsuarioP.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author nicks
+ */
+public class UsuarioP {
+    
+    private String matricula; 
+    private String usuario;
+    private String senha;
+
+    public UsuarioP(String matricula, String usuario, String senha) {
+        this.matricula = matricula;
+        this.usuario = usuario;
+        this.senha = senha;
+    }
+
+    public UsuarioP(String matricula, String senha) {
+        this.matricula = matricula;
+        this.senha = senha;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+    
+}
